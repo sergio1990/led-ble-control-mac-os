a650f97c6fb23ed897f659fa874dfde7cfc1f20b
Sergio Gernyak <sergeg1990@gmail.com>
2018-02-07 16:21:06 +0200

    Initial Commit

---
+++ LedBLEControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LedBLEControl
+//
+//  Created by Sergey Gernyak on 2/7/18.
+//  Copyright © 2018 Brain Amsterdam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LedBLEControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LedBLEControl
+//
+//  Created by Sergey Gernyak on 2/7/18.
+//  Copyright © 2018 Brain Amsterdam. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
