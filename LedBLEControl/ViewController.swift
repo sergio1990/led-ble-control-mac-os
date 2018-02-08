a0102ec2ca5b347512000565bf822106cd57a05a
Sergio Gernyak <sergey.gernyak@diymaxeda.com>
2018-02-08 14:55:54 +0200

    Starting BLE central manager
    Also added an ability to write and show debug messages

--- LedBLEControl/ViewController.swift
+++ LedBLEControl/ViewController.swift
@@ -7,13 +7,16 @@
 //
 
 import Cocoa
+import CoreBluetooth
 
 class ViewController: NSViewController {
-
+    fileprivate var centralManager: CBCentralManager?
+    @IBOutlet var logsTextView: NSTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        startCentralManager()
     }
 
     override var representedObject: Any? {
@@ -21,7 +24,50 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    func startCentralManager() {
+        let centralQueue = DispatchQueue(label: "com.wearebrain.LedBLEControlQueue", attributes: [])
+        centralManager = CBCentralManager(delegate: self, queue: centralQueue)
+    }
+    
+    func writeLogEntry(message: String) {
+        DispatchQueue.main.async {
+            let oldString = self.logsTextView.string
+            let newString = oldString + message
+            self.logsTextView.string = newString
+        }
+    }
 }
 
+extension ViewController : CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch (central.state) {
+        case .poweredOff:
+            self.writeLogEntry(message: "BLE discovering is powered off!")
+            //            self.clearDevices()
+            
+        case .unauthorized:
+            self.writeLogEntry(message: "BLE discovering is unauthorized!")
+            // Indicate to user that the iOS device does not support BLE.
+            break
+            
+        case .unknown:
+            self.writeLogEntry(message: "BLE discovering is unknown!")
+            // Wait for another event
+            break
+            
+        case .poweredOn:
+            self.writeLogEntry(message: "BLE discovering is powered on!")
+//            self.canScanning = true
+//            self.delegate?.canStartDiscover()
+            
+        case .resetting:
+            self.writeLogEntry(message: "BLE discovering is resetting!")
+            //            self.clearDevices()
+            
+        case .unsupported:
+            self.writeLogEntry(message: "BLE discovering is unsupported!")
+            break
+        }
+    }
+}
